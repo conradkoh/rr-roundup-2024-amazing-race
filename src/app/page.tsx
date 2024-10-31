3fc93cd9adc6ada42fa7cb495a66b7178d97bc73
conradkoh <conradkoh@gmail.com>
2024-11-01 01:51:31 +0800

    initial health bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,70 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import { useCallback, useState } from 'react';
 
 export default function Home() {
+  const [health, setHealth] = useState(100);
+  const takeDamage = useCallback(
+    (dmg: number) => {
+      if (health > 0) {
+        setHealth(Math.max(health - dmg, 0));
+      }
+    },
+    [health]
+  );
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <div className="h-screen min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="h-full w-full">
+          <div className="w-full h-full items-center justify-center game-font">
+            <div className="text-5xl text-center w-100">
+              Captain Chaos&apos; Lair
+            </div>
+            <div className="h-full flex flex-col justify-center items-center">
+              <div className="flex justify-center items-center text-5xl">
+                <HealthBar health={health} />
+              </div>
+              <div className="pt-20 w-full text-center flex flex-col items-center">
+                <div className="text-xl">DAMAGE CONTROLS</div>
+                <div className="pt-3 w-full flex items-center justify-center space-x-3">
+                  <button
+                    className="p-2 rounded-sm bg-gray-200"
+                    onClick={() => takeDamage(5)}
+                  >
+                    HEAD (-5 HP)
+                  </button>
+                  <button
+                    className="p-2 rounded-sm bg-gray-200"
+                    onClick={() => takeDamage(2)}
+                  >
+                    BODY (-2 HP)
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          <div>Bethel RR Round Up 2024</div>
+        </footer>
+      </div>
+    </>
+  );
+}
+function asset(name: string) {
+  return '/assets/' + name;
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+function HealthBar(props: { health: number }) {
+  return (
+    <>
+      <Image src={asset('heart.png')} alt="Heart" width={100} height={100} />
+      <div className="ml-10 health-bar">
+        <div className="health-fill" style={{ width: `${props.health}%` }} />
+      </div>
+      <div className="pl-5 health-text font-[family-name:var(--font-press-start)]">
+        {props.health}
+      </div>
+    </>
   );
 }
