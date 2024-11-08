fde99795b00f934dfcf3e675028ac5d4a1e2d6d5
conradkoh <conradkoh@gmail.com>
2024-11-08 11:12:31 +0800

    added transitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,45 @@
 'use client';
+import { ConditionalRender } from '@/app/components/condition/ConditionalRender';
 import { MainContentSection } from '@/app/components/sections/main-content';
+import { api } from '@convex/_generated/api';
+import { useQuery } from 'convex/react';
 
 export default function Home() {
+  const gameState = useQuery(api.gameState.get);
+  if (gameState === undefined) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      <div className="h-screen w-screen min-h-screen pt-8 font-[family-name:var(--font-geist-sans)]">
-        <main className="h-full w-full">
-          <div className="game-font h-full w-full flex flex-col">
-            <div className="text-5xl text-center w-100">
-              Captain Chaos&apos; Lair
-            </div>
-            <div className="pt-10 flex flex-col flex-grow">
-              <MainContentSection />
-            </div>
-          </div>
-        </main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-end">
-          <div className="text-sm bg-gray-100 border border-gray-200 rounded-md p-2 fixed bottom-3 opacity-50">
-            Bethel RR Round Up 2024
-          </div>
-        </footer>
+      {/* Waiting Room Screen */}
+      <div
+        className={`top-0 left-0 waiting-room-root h-screen w-screen flex flex-col items-center justify-center ${gameState.status.type === 'started' ? 'fadeout' : ''}`}
+      >
+        <div className="text-red-600 game-font text-9xl text-shadow-lg p-5 pb-1 rounded-md">
+          CHAOS LAIR
+        </div>
       </div>
+      {/* Main Screen */}
+      <ConditionalRender renderIf={() => gameState.status.type === 'started'}>
+        <div
+          className={`top-0 left-0 h-screen w-screen min-h-screen pt-8 font-[family-name:var(--font-geist-sans)] fadein`}
+        >
+          <main className="h-full w-full">
+            <div className="game-font h-full w-full flex flex-col">
+              <div className="text-5xl text-center w-100">Captain Chaos</div>
+              <div className="pt-10 flex flex-col flex-grow">
+                <MainContentSection />
+              </div>
+            </div>
+          </main>
+          <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-end">
+            <div className="text-sm bg-gray-100 border border-gray-200 rounded-md p-2 fixed bottom-3 opacity-50">
+              Bethel RR Round Up 2024
+            </div>
+          </footer>
+        </div>
+      </ConditionalRender>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      textShadow: {
+        'default': '2px 2px 4px rgba(0, 0, 0, 0.1)',
+        'md': '3px 3px 6px rgba(0, 0, 0, 0.2)',
+        'lg': '4px 4px 8px rgba(0, 0, 0, 0.3)',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }: { addUtilities: any }) {
+      const newUtilities = {
+        '.text-shadow': {
+          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)',
+        },
+        '.text-shadow-md': {
+          textShadow: '3px 3px 6px rgba(0, 0, 0, 0.2)',
+        },
+        '.text-shadow-lg': {
+          textShadow: '4px 4px 8px rgba(0, 0, 0, 0.3)',
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 };
 export default config;
